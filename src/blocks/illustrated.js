e1a91d3a13615f85838aced53d3979c9c4fae204
Arthur Balagne <arthur.balagne@pixel-cookers.com>
2015-06-24 09:52:07 +0200

    removed spam on buttons

--- src/blocks/illustrated.js
+++ src/blocks/illustrated.js
@@ -14,16 +14,14 @@ var Slider = require('../helpers/slider.class.js');
 var blockTemplate = _.template([
 '<div class="st-text-block" contenteditable="true">',
     '<div class="illustrated">',
-    '<figure contenteditable="false">',
+    '<figure class="empty" contenteditable="false">',
         '<img src="<%= imgSrc %>" alt="<%= imgAlt %>">',
     '</figure>',
     '<div class="title" style="<%= titleStyle %>"><%= titleText %></div>',
     '<div class="text"><%= text %></div>',
     '</div>',
-'</div>',
-].join("\n"));
-
-
+'</div>'
+].join('\n'));
 
 var colorpicker =  _.template([
     '<select class="colors">',
@@ -33,7 +31,7 @@ var colorpicker =  _.template([
         '<option class="bcg-brown"  value="#B35C1E">Marron</option>',
         '<option class="bcg-grey"  value="#DADADA">Gris</option>',
     '</select>'
-].join("\n"));
+].join('\n'));
 
 var mediaTemplate = _.template([
     '<div class="media">',
@@ -42,16 +40,16 @@ var mediaTemplate = _.template([
         '<div class="center"></div>',
         '<div class="right"></div>',
         '<div class="footer"></div>',
-    '</div>',
+    '</div>'
     ].join('\n'));
 
 function generateContainer(icons) {
     if (icons === undefined) {
-        return;
+        return [];
     }
     var tabIcons = [];
     Object.keys(icons).forEach(function(k){
-        var single = _.template('<img src="<%= icon %>" alt="placeholder">')({ icon: icons[k] });
+        var single = _.template('<img src="<%= icon %>" alt="<%= alt %>">')({ icon: icons[k].src, alt: icons[k].copyright  });
         tabIcons.push(single);
     });
     return tabIcons;
@@ -59,11 +57,7 @@ function generateContainer(icons) {
 
 function changePictureOnClick($selected, $block) {
     var selectedSrc =  $selected.attr('src');
-    $block.find('figure img').attr('src', selectedSrc)
-}
-
-function handleDrop() {
-
+    $block.find('figure img').attr('src', selectedSrc);
 }
 
 module.exports = Block.extend({
@@ -81,6 +75,7 @@ module.exports = Block.extend({
             slug: 'change-color',
             eventTrigger: 'change',
             fn: function(e) {
+                e.preventDefault();
                 var color = this.$el.find('.colors').find('option:selected').attr('value');
                 this.$el.find('.title').css('color', color);
             },
@@ -91,70 +86,84 @@ module.exports = Block.extend({
             eventTrigger: 'click',
             fn: function(e) {
                 e.preventDefault();
+                e.stopPropagation();
                 var tabIcons = generateContainer([
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-aston-martin.jpeg',
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-audi.jpeg',
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-bmw.png',
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-lotus.jpeg',
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-koenigsegg.jpeg',
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-tesla-motors.jpeg',
-                    'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-ford.jpeg'
+                    {
+                        src: 'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-audi.jpeg',
+                        copyright: 'Audi'
+                    },
+                    {
+                        src: 'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-bmw.png',
+                        copyright: 'Bmw'
+                    },
+                    {
+                        src: 'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-lotus.jpeg',
+                        copyright: 'Lotus'
+                    },
+                    {
+                        src: 'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-tesla-motors.jpeg',
+                        copyright: 'Tesla'
+                    },
+                    {
+                        src: 'http://www.voituresdeprestige.fr/media/cache/resolve/logo_marque/uploads/images/marque-ford.jpeg',
+                        copyright: 'Ford'
+                    }
                 ]);
-                this.$el.find('.illustrated').append(mediaTemplate());
-
-                var that = this;
-                $('.close-media').on('click', function(){
-                    that.$el.find('.media').remove();
-                });
-
-                var slider = new Slider(tabIcons);
-                slider.init({
-                    itemsPerSlide: 8,
-                    increment: 1,
-                    controls: false,
-                    contents: tabIcons,
-                    container: that.$el.find('.illustrated .media .center')
-                });
-                this.$el.find('.illustrated .media .footer').append('<div  contenteditable="false" class="droppable">Uploader et associer une nouvelle image</div>')
-
-                var $sliderImages = this.$el.find('.illustrated .media .st-slider-slide img')
-                $.each($sliderImages, function(){
-                    $(this).on('click', function(){
-                        changePictureOnClick($(this), that.$editor);
+                if(this.$el.find('.illustrated .media').val() === undefined) {
+                    this.$el.find('.illustrated').append(mediaTemplate());
+                    var that = this;
+                    $('.close-media').on('click', function(){
                         that.$el.find('.media').remove();
                     });
-                });
-                this.$el.find('.droppable').on('dragover dragenter', function(ev){
-                    ev.preventDefault();
-                    ev.stopPropagation();
-                });
-                this.$el.find('.droppable').on('drop', function(ev){
-                    ev.preventDefault();
-                    ev.stopPropagation();
-                    var file = ev.originalEvent.dataTransfer.files[0];
-                    that.uploader(
-                        file,
-                        function(data) {
-                            that.setData(data);
-                            that.ready();
-                        },
-                        function(error) {
-                            that.addMessage(i18n.t('blocks:image:upload_error'));
-                            that.ready();
-                        }
-                    );
-                });
 
+                    var slider = new Slider(tabIcons);
+                    slider.init({
+                        itemsPerSlide: 8,
+                        increment: 1,
+                        controls: false,
+                        contents: tabIcons,
+                        container: that.$el.find('.illustrated .media .center')
+                    });
+                    this.$el.find('.illustrated .media .footer').append('<div contenteditable="false" class="droppable">Uploader et associer une nouvelle image</div>');
+
+                    var $sliderImages = this.$el.find('.illustrated .media .st-slider-slide img');
+                    $.each($sliderImages, function(){
+                        $(this).on('click', function(){
+                            changePictureOnClick($(this), that.$editor);
+                            that.$el.find('.media').remove();
+                        });
+                    });
+                    this.$el.find('.droppable').on('dragover dragenter', function(ev){
+                        ev.preventDefault();
+                        ev.stopPropagation();
+                    });
+                    this.$el.find('.droppable').on('drop', function(ev){
+                        ev.preventDefault();
+                        ev.stopPropagation();
+                        var file = ev.originalEvent.dataTransfer.files[0];
+                        that.uploader(
+                            file,
+                            function(data) {
+                                that.setData(data);
+                                that.ready();
+                            },
+                            function(error) {
+                                that.addMessage(i18n.t('blocks:image:upload_error'));
+                                that.ready();
+                            }
+                        );
+                    });
+                }
             },
             html: '<span>Icon</span>'
         }
     ],
     editorHTML: blockTemplate({
-        text: "Votre texte",
-        titleText: "Votre titre",
-        titleStyle: "",
-        imgSrc: "",
-        imgAlt: ""
+        text: 'Votre texte',
+        titleText: 'Votre titre',
+        titleStyle: '',
+        imgSrc: '',
+        imgAlt: ''
       }),
     icon_name: 'text',
 
@@ -163,16 +172,15 @@ module.exports = Block.extend({
             text: data.text,
             titleText: data.title.text
 
-        }
+        };
         var tpl = '';
 
-        templateObject.titleStyle =  data.title.style !== undefined ? data.title.style : '';
+        templateObject.titleStyle = data.title.style !== undefined ? data.title.style : '';
         if (data.img !== undefined) {
-            templateObject.imgSrc =  data.img.src;
-            templateObject.imgAlt =  data.img.alt;
+            templateObject.imgSrc = data.img.src;
+            templateObject.imgAlt = data.img.alt;
         }
-        tpl = blockTemplate()
-        debugger;
+        tpl = blockTemplate();
         this.getTextBlock().html(stToHTML(data.text, this.type));
     },
     setData: function(data) {
@@ -180,22 +188,24 @@ module.exports = Block.extend({
         var img = '';
         var title = '';
         var text = '';
+        var obj = {};
         if (content.find('.text').html().length > 0) {
             img = this.$el.find('figure img');
-            if (img !== undefined) {
+            if (img.attr('src') !== '') {
                 data.img = {};
-                var obj = {
+                obj = {
                     src: img.attr('src'),
                     alt: img.attr('alt')
-                }
+                };
                 Object.assign(data.img, obj);
+                content.find('figure').removeClass('empty');
             }
             title = this.$el.find('.title');
             data.title = {};
-                var obj = {
-                    text: title.val(),
-                    style: title.attr('style')
-                }
+            obj = {
+                text: title.val(),
+                style: title.attr('style')
+            };
             Object.assign(data.title, obj);
 
             text = this.$el.find('.text').html();
@@ -205,10 +215,13 @@ module.exports = Block.extend({
     },
 
     toMarkdown: function(markdown) {
-        return markdown.replace(/^(.+)$/mg,"$1");
+        return markdown.replace(/^(.+)$/mg, '$1');
     },
 
     onBlockRender: function() {
-    },
+        $('.illustrated figure').one('click', function(){
+            $('.st-block-control-ui-btn--change-picture').trigger('click');
+        });
+    }
 
 });
